ee86c01303225cb6d2363dab931d9986f31aa95a
Mohammed Faizan Ul Islam <141151023+NotDasans@users.noreply.github.com>
2023-10-30 21:07:25 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,49 @@
-# Clearview
+# ClearView Blogging
+#### Video Demo:  <https://youtu.be/gpypFxhqMrU>
+## Description:
+### Welcome to ClearView Blogging!
+### Introduction:
+This website allows users to login, register, post, comment and search various blogs. This project uses HTML, CSS, JavaScript, Bootstrap, Python and Flask framework. It has a user interface that allows users to have an easy and comfortable time accessing the various features of the website.
+The project contains the following folders and files:
+*	Flask_session
+*	Static: which contains some images used in the website and styles.css file.
+*	Templates: this folder contains all the various html files which are rendered in the website
+*	app.py: this file contains all the functions for different routes and backend of the website.
+*	clearview.db: this is a database which contains all the data regarding users, blogs and comments.
+*	helpers.py: this file contains helper functions like login_required.
+*	README.md: this file
+
+### Layout:
+The template layout.html contains the basic layout which is rendered into all other templates using Jinja syntax. It contains the meta data in head tag and the navigation bar and footer in body tag.\
+The navigation bar allows us to access various pages of the website like home, accounts, about, contact it also contains a search bar and access to login, register and logout (if logged in already).\
+The footer contains links to various social media in thee form of buttons with icons and copyright information.
+
+### Login and Register:
+The users can login and register under their respective tabs on the navigation bar. The login and register function handle all the corner cases in case any input is invalid (example: wrong password or username in case of login or already in use username in case of register etc.) or left blank and displays the corresponding error message on the same page. Once the users have logged in, they can access the login required features that is the account page (which allows users to post or see their previous blogs) and comment feature (rest of the features can be accessed without logging in).\
+The login.html template presents a form which asks for the user’s username and password and works to login if the information is correct and displays and error message below the form and reloading the form to allow the user to enter again. It uses data from the clearview.db database.
+The register.html template presents a form which asks for the user’s username name password and another field to confirm password and works to register the user if all requirements are meant and displays an error message like that of login page and reloads the form.
+>For security reasons the password is hashed appropriately before storing in the database.
+### Logout:
+The logout route simply clears the session and logs the user out.
+### Index:
+The index or the homepage consists of a carousel which displays some awards and achievements and consists of the three of the latest blogs which are in descending order of their date and time of upload. The blogs are displayed in cards which contain the title, name of the writer, an image, and a preview of the content inside it on clicking on the “go to page” button it takes you to the required blog.
+### Account:
+The account page can be accessed from the navigation. It is required to login in order to access this page if not logged in it redirects to the login page. This page consists of a form that can be filled to make a new blog it asks for a title, an image URL and content of a blog on clicking the post button posts the blog. It also contains the blogs posted by the logged in user in same manner as on the index page (i.e., card with go to button to access the blog).
+### Contact Us:
+The contact us page consists of a form that can be filled for giving feedback or asking any queries by the user it will be submitted and stored in the database. It also contains the contact details like phone number, email and address of the company.
+### About Us:
+The about us page consists of the general information about the company’s vision and values, it also contains the company moto.
+### Search:
+The search bar can be accessed from the navigation bar it brings up any blogs or articles in descending order of date and time of upload whose title is like the text given in the search bar in a similar manner of that of the index page (i.e., card with go to button to access the blog). It displays no result when no results match.
+### Blog Page:
+The blog page can be accessed by the go to button in the cards present in either index, accounts, or search results page. These bring the pages by the “get” method that means that these pages can also be accessed from URLs that are shared. This page consists of an image, name of writer, date and time of upload and the content of the blog below this it consists of the comment feature.
+### Comments:
+The user if logged in can comment on any of the blogs. The comments are displayed in descending order of their upload. The comments contain name of the commenter and date and time of upload on left and the comment on the right.\
+Comments are unique to every blog.
+### clearview database
+The clearview database consists of four tables:
+1.	Users: the users table consists of the necessary information about all the users. The password is hashed.
+1.	Post: the post table consists of all the necessary information about all the posts that have been posted by various users.
+1.	Feedback: the feedback table stores all the information that have been given the feedback/queries form in contact us page.
+1.	Comment: the comment table consists of the data about all the comments with necessary foreign keys like user_id and post_id.
+
---
+++ app.py
@@ -0,0 +1,292 @@
+import os
+
+from cs50 import SQL
+from flask import Flask, flash, redirect, render_template, request, session
+from flask_session import Session
+from werkzeug.security import check_password_hash, generate_password_hash
+
+from helpers import login_required  # lookup, usd
+import json
+
+# default password for all test accounts abcd
+
+# Configure application
+app = Flask(__name__)
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Configure CS50 Library to use SQLite database
+db = SQL("sqlite:///clearview.db")
+
+
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    # Query the database for latest 3 articles
+    articles = db.execute(
+        "SELECT id, image, title, name, SUBSTR(content, 1, 220) AS limited_content FROM post ORDER BY date_n_time DESC LIMIT 3"
+    )
+    return render_template("index.html", latest=articles)
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    """Log user in"""
+
+    # Forget any user_id
+    session.clear()
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            return render_template("login.html", apology="must provide username")
+
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            return render_template("login.html", apology="must provide password")
+
+        # Query database for username
+        rows = db.execute(
+            "SELECT * FROM users WHERE username = ?", request.form.get("username")
+        )
+
+        # Ensure username exists and password is correct
+        if len(rows) != 1 or not check_password_hash(
+            rows[0]["hash"], request.form.get("password")
+        ):
+            return render_template("login.html", apology="invalid username or password")
+
+        # Remember which user has logged in
+        session["user_id"] = rows[0]["id"]
+
+        # Redirect user to home page
+        return redirect("/")
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("login.html")
+
+
+@app.route("/logout")
+def logout():
+    """Log user out"""
+
+    # Forget any user_id
+    session.clear()
+
+    # Redirect user to login form
+    return redirect("/")
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    """Register user"""
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            return render_template("register.html", apology="must provide username")
+        # Ensure name was submitted
+        if not request.form.get("name"):
+            return render_template("register.html", apology="must provide name")
+
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            return render_template("register.html", apology="must provide password")
+
+        # Ensure confirmation was submitted
+        elif not request.form.get("confirmation"):
+            return render_template("register.html", apology="must provide confirmation")
+
+        # Ensure password and confirmation match
+        elif request.form.get("password") != request.form.get("confirmation"):
+            return render_template("register.html", apology="password not confirmed")
+        # Query database for username
+        check = db.execute(
+            "SELECT * FROM users WHERE username = ?", request.form.get("username")
+        )
+        if len(check) == 0:
+            db.execute(
+                "INSERT INTO users (username, hash, name) VALUES(?, ?, ?)",
+                request.form.get("username"),
+                generate_password_hash(request.form.get("password")),
+                request.form.get("name"),
+            )
+            rows = db.execute(
+                "SELECT * FROM users WHERE username = ?", request.form.get("username")
+            )
+            # Remember which user has logged in
+            session["user_id"] = rows[0]["id"]
+            # Redirect user to home page
+            return redirect("/")
+        else:
+            # If username already exists
+            return render_template("register.html", apology="username already exists")
+    else:
+        # When method == "POST"
+        return render_template("register.html")
+
+
+@app.route("/contact", methods=["GET", "POST"])
+def contact():
+    """ Generate Contact Us """
+    return render_template("contact.html")
+
+
+@app.route("/form_submit", methods=["POST"])
+def form_submit():
+    # Submit feedback form into database
+    db.execute(
+        "INSERT INTO feedback (name, email, feed) VALUES (?, ?, ?)",
+        request.form.get("name"),
+        request.form.get("email"),
+        request.form.get("feed"),
+    )
+    return render_template("contact.html")
+
+
+@app.route("/about_us", methods=["GET"])
+def about_us():
+    """ Generate About Us """
+    return render_template("about_us.html")
+
+
+@app.route("/account", methods=["GET"])
+# Login required
+@login_required
+def account():
+    # Query the database for articles written by the user
+    articles = db.execute(
+        "SELECT id, image, title, name, SUBSTR(content, 1, 220) AS limited_content FROM post WHERE user_id = ? ORDER BY date_n_time DESC",
+        session["user_id"],
+    )
+    return render_template("account.html", art=articles)
+
+
+@app.route("/new_post", methods=["GET", "POST"])
+# Post the article written by the user
+def new_post():
+    if request.method == "POST":
+        # Check if title provided
+        if not request.form.get("title"):
+            return render_template("account.html", apology="must have title")
+        # Check if content provided
+        elif not request.form.get("content"):
+            return render_template("account.html", apology="must have content")
+        name = db.execute("SELECT name FROM users WHERE id=?", session["user_id"])
+        n_ = name[0]["name"]
+        date = db.execute("SELECT DATETIME('NOW') AS current_date")
+        d_ = date[0]["current_date"]
+        # Add the article into database
+        db.execute(
+            "INSERT INTO post (name, image, date_n_time, content, title, user_id) VALUES(?, ?, ?, ?, ?, ?)",
+            n_,
+            request.form.get("image"),
+            d_,
+            request.form.get("content"),
+            request.form.get("title"),
+            session["user_id"],
+        )
+        return redirect("/account")
+
+@app.route("/search", methods=["POST"])
+def search():
+    search = request.form.get("search")
+    # Query the database for titles like entered in search
+    articles = db.execute(
+        "SELECT id, image, title, name, SUBSTR(content, 1, 220) AS limited_content FROM post WHERE title LIKE ? ORDER BY date_n_time DESC LIMIT 10",
+        "%" + search + "%",
+    )
+    # if no articles found
+    if not articles:
+        return render_template("search.html", results="No results")
+    return render_template("search.html", res=articles)
+
+
+@app.route("/article", methods=["GET"])
+def article():
+    return render_template("article.html")
+
+@app.route("/go_to_page", methods=["POST", "GET"])
+def go_to_page():
+    if request.method == "GET":
+        # Query the database for the article
+        rendering = db.execute(
+            "SELECT image, title, name, date_n_time, content FROM post content WHERE id=?",
+            request.args.get("article_id"),
+        )
+        render = rendering[0]
+        ccc = json.dumps(render)
+        # Query the database for comments on that article
+        comment = db.execute(
+            "SELECT name, date_n_time, content FROM comments WHERE post_id=? ORDER BY date_n_time DESC",
+            request.args.get("article_id"),
+        )
+        return render_template(
+            "article.html",
+            cc=render,
+            article_id=request.args.get("article_id"),
+            ccc=ccc,
+            comment=comment,
+        )
+    else:
+        rendering = db.execute(
+            "SELECT image, title, name, date_n_time, content FROM post content WHERE id=?",
+            request.args.get("article_id"),
+        )
+        render = rendering[0]
+        print(rendering)
+        print(render)
+        ccc = json.dumps(render)
+        return render_template(
+            "article.html",
+            cc=render,
+            article_id=request.form.get("article_id"),
+            ccc=ccc,
+        )
+
+
+@app.route("/comment", methods=["POST"])
+# Post the comment by the user
+# Login in required to comment
+@login_required
+def comment():
+    # Name from session
+    name = db.execute("SELECT name FROM users WHERE id=?", session["user_id"])
+    n_ = name[0]["name"]
+    # Date and Time of posting
+    date = db.execute("SELECT DATETIME('NOW') AS current_date")
+    d_ = date[0]["current_date"]
+    # Add the comment into database
+    db.execute(
+        "INSERT INTO comments (name, date_n_time, content, person_id, post_id) VALUES(?, ?, ?, ?, ?)",
+        n_,
+        d_,
+        request.form.get("comment"),
+        session["user_id"],
+        request.form.get("article_id"),
+    )
+    ccc = json.loads(request.form.get("ccc"))
+    ddd = json.dumps(ccc)
+    comment = db.execute(
+        "SELECT name, date_n_time, content FROM comments WHERE post_id=? ORDER BY date_n_time DESC",
+        request.form.get("article_id"),
+    )
+    return render_template(
+        "article.html",
+        cc=ccc,
+        article_id=request.form.get("article_id"),
+        ccc=ddd,
+        comment=comment,
+    )
